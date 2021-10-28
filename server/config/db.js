d94904a7d9387059a735618fff9cca4dceb669be
Ana Pereira <aperei46@my.centennialcollege.ca>
2021-10-28 02:29:14 -0400

    first commit-midterm edited

---
+++ server/config/db.js
@@ -0,0 +1,4 @@
+module.exports = {
+  //local MongoDB deployment ->
+  "URI": "mongodb+srv://annyper:x5xL1kuL7RVMaxTy@mongodbserver.9eprd.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+};
